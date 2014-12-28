765901c814ee0ae51e4375aeb6ae8e93f744674e
qinwf <mail@qinwenfeng.com>
2014-12-28 20:58:05 +0800

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,5 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
-
-worker(:testitmix)
-worker(:testithmm,"hmm")
-worker(:testitqu,"query")
-worker(:testitmp)
-
-deleteworker(jieba.testitmix)
-deleteworker(jieba.testitqu)
-deleteworker(jieba.testitmp)
-deleteworker(jieba.testithmm)
+cutter = worker()
+delete_worker(cutter)
